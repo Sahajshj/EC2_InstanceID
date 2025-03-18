fbab14c8f4cbabe424314d32b02add3c83a97c1a
Sahajshj <133161902+Sahajshj@users.noreply.github.com>
2025-03-18 22:22:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 // Serve static frontend files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 
 // Function to fetch instance ID from AWS metadata
 const getInstanceId = () => {
